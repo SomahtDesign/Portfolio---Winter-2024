174b0aabbe378b4ebffa7cfcd9e786be5c13bce0
SomahtDesign <somaht.network@gmail.com>
2024-10-30 22:03:42 +0100

    Photography

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ setInterval(() => {
     greeting.textContent = greetings[index];
     index = (index + 1) % greetings.length;
 }, 3000);
+
+// Fonction pour détecter l'orientation des images
+function detectImageOrientation() {
+    const images = document.querySelectorAll('.photo-item');
+
+    images.forEach((img) => {
+        const image = new Image();
+        image.src = img.src;
+
+        image.onload = () => {
+            if (image.width > image.height) {
+                img.classList.add('landscape'); // Ajouter la classe paysage
+                img.classList.add('highlight'); // Vous pouvez changer cela selon votre logique
+            } else {
+                img.classList.add('portrait'); // Ajouter la classe portrait
+                img.classList.remove('highlight'); // Assurez-vous que 'highlight' n'est pas appliqué
+            }
+        };
+    });
+}
+
+// Appeler la fonction au chargement de la page
+window.onload = detectImageOrientation;
+
